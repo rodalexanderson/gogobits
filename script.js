d2b525e6506549e3526d0ee8ab50afdadbb9f0b6
rodalexanderson <alexanderson.ibarra@gmail.com>
2023-05-12 00:29:41 -0600

    changes in services style, contact form modified to new template in emailjs

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function sendMail() {
-    emailjs.sendForm('service_aokww4f', 'template_tx11two', '#myForm')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
-}
+    emailjs.sendForm('service_aokww4f', 'template_k5cslp8', '#myForm')
+    .then(function() {
+      console.log('SUCCESS!')
+  }, function(error) {
+      console.log('FAILED...', error)
+  });
+    }
